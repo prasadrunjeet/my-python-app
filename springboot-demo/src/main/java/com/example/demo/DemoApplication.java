2a80ad42149ef6777ef29470db7f8d2c1a404d37
Runjeet Prasad <rprasad44@outlook.com>
2025-06-08 22:54:15 +0530

    Spring Bott Application

---
+++ springboot-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+}
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
