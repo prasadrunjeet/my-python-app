526291d5971df0581e41e027a5ab0b4fe76e314a
Runjeet Prasad <rprasad44@outlook.com>
2025-06-15 18:17:17 +0530

    BG Updated

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -15,7 +15,7 @@ public class HelloController {
                "<head>" +
                "    <title>Mahatma Gandhi</title>" +
                "    <style>" +
-               "        body { background-color:rgb(83, 72, 133); font-family: Arial, sans-serif; padding: 30px; }" +
+               "        body { background-color:rgb(184, 185, 120); font-family: Arial, sans-serif; padding: 30px; }" +
                "        h1 { color: #2e8b57; text-decoration: underline; }" +
                "        h2 { color:rgb(43, 226, 156); text-decoration: underline dotted; }" +
                "        p { color: #333; line-height: 1.6; font-size: 18px; }" +
